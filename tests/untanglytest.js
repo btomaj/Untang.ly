5a27d171f4e72413e6153432d0496888782b4821
poetry by <tbayat@tomaj.co>
2019-04-27 12:27:57 +1000

    Completed refactoring of Models and Controller
    Completed:
    U.Shape
    U.Node
    U.Canvas
    U.CanvasController

--- tests/untanglytest.js
+++ tests/untanglytest.js
@@ -17,33 +17,39 @@ var nodeUnit = require("nodeUnit"),
 
             var square = new U.Shape("smallSquare", "largeSquare");
 
-            assert(square.path === "largeSquare", "Shape does not store shape");
+            assert(square.shape === "largeSquare", "Shape does not store shape");
         },
         "test U.Shape works correctly when invoked without \"new\"": function () {
             "use strict";
 
             var square = U.Shape("smallSquare", "largeSquare");
 
-            assert(square.path === "largeSquare" && square.preview === "smallSquare", "Shape breaks when invoked without \"new\"");
+            assert(square.shape !== undefined && square.preview !== undefined, "Shape breaks when invoked without \"new\"");
         }
     },
 
     NodeTestSuite = {
-        "test U.Node stores coordinates": function () {
+        setUp: function () {
             "use strict";
 
-            var node = new U.Node(1, 2);
+            this.node = new U.Node(1, 2);
+            this.shape = new U.Shape("preview", "path");
 
-            assert(node.x === 1 && node.y === 2, "U.Node does not store coordinates");
         },
-        "test Node.draw records Shape object": function () {
+        "test Node.draw() assigns Shape object to Node object": function () {
             "use strict";
 
-            var node = new U.Node(0, 0),
-                shape = new U.Shape("preview", "path");
+            this.node.draw(this.shape);
 
-            node.draw(shape);
-            assert(node.shape === shape, "Node.draw failed to record Shape object");
+            assert(this.node.shape === this.shape, "Node.draw failed to record Shape object");
+        },
+        "test Node.undraw() removes Shape object from Node object": function () {
+            "use strict";
+
+            this.node.draw(this.shape);
+            this.node.undraw();
+
+            assert(this.node.shape === undefined, "Node.undraw() failed to remove Shape object");
         }
     },
 
@@ -52,11 +58,7 @@ var nodeUnit = require("nodeUnit"),
             "use strict";
 
             this.canvas = new U.Canvas();
-
-            this.node = {
-                x: 0,
-                y: 0
-            };
+            this.node = new U.Node(0, 0);
         },
         "test U.Canvas.add stores Node object in grid": function () {
             "use strict";
@@ -68,27 +70,159 @@ var nodeUnit = require("nodeUnit"),
         "test U.Canvas.remove removes Node object from grid": function () {
             "use strict";
 
-            this.canvas.grid = [];
-            this.canvas.grid[this.node.x] = [];
-            this.canvas.grid[this.node.x][this.node.y] = this.node;
-
+            this.canvas.add(this.node);
             this.canvas.remove(this.node);
             assert(this.canvas.grid[this.node.x][this.node.y] === undefined, "U.Canvas.remove does not remove Node object from grid");
+        },
+        "test U.Canvas.add does not overwrite existing Node": function () {
+            "use strict";
+
+            var node_original = new U.Node(0, 0),
+                node_new = new U.Node(0, 0),
+                shape = new U.Shape("preview", "path");
+
+            this.canvas.add(node_original);
+            node_original.draw(shape);
+            this.canvas.add(node_new);
+
+            assert(this.canvas.grid[0][0].shape === shape, "Node was overwritten");
         },
         tearDown: function () {
             "use strict";
 
             delete this.node;
-
             delete this.canvas;
         }
     },
 
     CanvasControllerTestSuite = {
-        "test U.CanvasController": function () {
+        setUp: function () {
             "use strict";
 
-            var canvasController = new U.CanvasController();
+            this.canvas = new U.Canvas();
+            this.node = new U.Node(0, 0);
+            this.shape = new U.Shape("preview", "path");
+            this.canvasController = new U.CanvasController(this.canvas);
+
+        },
+        "test U.CanvasController returns model": function () {
+            "use strict";
+
+            assert(this.canvasController.model === this.canvas, "U.CanvasController does not return model");
+        },
+        "test U.CanvasController.draw() assigns Shape object to Node object": function () {
+            "use strict";
+
+            this.canvas.add(this.node);
+            this.canvasController.draw(this.node, this.shape);
+
+            assert(this.canvas.grid[this.node.x][this.node.y].shape === this.shape, "Shape object was not assigned to Node object");
+        },
+        "test U.CanvasController.draw() adds new Node objects around the drawn Node object": function () {
+            "use strict";
+
+            this.canvas.add(this.node);
+            this.canvasController.draw(this.node, this.shape);
+
+            assert(this.canvas.grid[this.node.x][this.node.y + 1] !== undefined, "No Node found north of drawn Node");
+            assert(this.canvas.grid[this.node.x + 1][this.node.y] !== undefined, "No Node found east of drawn Node");
+            assert(this.canvas.grid[this.node.x][this.node.y - 1] !== undefined, "No Node found south of drawn Node");
+            assert(this.canvas.grid[this.node.x + 1][this.node.y] !== undefined, "No Node found west of drawn Node");
+        },
+        "test U.CanvasController.undraw() removes Shape object from Node object": function () {
+            "use strict";
+
+            this.canvas.add(this.node);
+            this.canvasController.draw(this.node, this.shape);
+            this.canvasController.undraw(this.node);
+
+            assert(this.canvas.grid[this.node.x][this.node.y].shape === undefined, "Shape object was not removed from Node object");
+        },
+        "test U.CanvasController.undraw() removes Node objects around undrawn Node object": function () {
+            "use strict";
+
+            this.canvas.add(this.node);
+            this.canvasController.draw(this.node, this.shape);
+            this.canvasController.undraw(this.node);
+
+            assert(this.canvas.grid[this.node.x][this.node.y + 1] === undefined, "Node north of removed Node was not removed");
+            assert(this.canvas.grid[this.node.x + 1][this.node.y] === undefined, "Node east of removed Node was not removed");
+            assert(this.canvas.grid[this.node.x][this.node.y - 1] === undefined, "Node south of removed Node was not removed");
+            assert(this.canvas.grid[this.node.x + 1][this.node.y] === undefined, "Node west of removed Node was not removed");
+        },
+        "test U.CanvasController.remove() removes Node object from Canvas object": function () {
+            "use strict";
+
+            this.canvas.add(this.node);
+            this.canvasController.remove(this.node);
+
+            assert(this.canvas.grid[this.node.x][this.node.y] === undefined, "Node object was not removed from Canvas object");
+        },
+        "test U.CanvasController.remove() does not remove Node object with assigned Shape object": function () {
+            "use strict";
+
+            this.canvas.add(this.node);
+            this.canvasController.draw(this.node, this.shape);
+            this.canvasController.remove(this.node);
+
+            assert(this.canvas.grid[this.node.x][this.node.y] === this.node, "Node object with assigned Shape object was removed from Canvas object");
+        },
+        "test U.CanvasController.remove() does not remove Node object adjacent to northern Node object with attached Shape object": function () {
+            "use strict";
+
+            var north = new U.Node(this.node.x, this.node.y + 1);
+
+            this.canvas.add(this.node);
+            this.canvas.add(north);
+            this.canvasController.draw(north, this.shape);
+            this.canvasController.remove(this.node);
+
+            assert(this.canvas.grid[this.node.x][this.node.y] === this.node, "Node object adjacent to northern Node object with assigned Shape object was removed");
+        },
+        "test U.CanvasController.remove() does not remove Node object adjacent to eastern Node object with attached Shape object": function () {
+            "use strict";
+
+            var east = new U.Node(this.node.x + 1, this.node.y);
+
+            this.canvas.add(this.node);
+            this.canvas.add(east);
+            this.canvasController.draw(east, this.shape);
+            this.canvasController.remove(this.node);
+
+            assert(this.canvas.grid[this.node.x][this.node.y] === this.node, "Node object adjacent to eastern Node object with assigned Shape object was removed");
+        },
+        "test U.CanvasController.remove() does not remove Node object adjacent to southern Node object with attached Shape object": function () {
+            "use strict";
+
+            var south = new U.Node(this.node.x, this.node.y - 1);
+
+            this.canvas.add(this.node);
+            this.canvas.add(south);
+            this.canvasController.draw(south, this.shape);
+            this.canvasController.remove(this.node);
+
+            assert(this.canvas.grid[this.node.x][this.node.y] === this.node, "Node object adjacent to southern Node object with assigned Shape object was removed");
+        },
+        "test U.CanvasController.remove() does not remove Node object adjacent to western Node object with attached Shape object": function () {
+            "use strict";
+
+            var west = new U.Node(this.node.x - 1, this.node.y);
+
+            this.canvas.add(this.node);
+            this.canvas.add(west);
+            this.canvasController.draw(west, this.shape);
+            this.canvasController.remove(this.node);
+
+            assert(this.canvas.grid[this.node.x][this.node.y] === this.node, "Node object adjacent to western Node object with assigned Shape object was removed");
+        },
+        //TODO "test U.CanvasController.remove() does not remove the last remaining Node object": function () {},
+        tearDown: function () {
+            "use strict";
+
+            delete this.canvas;
+            delete this.node;
+            delete this.shape;
+            delete this.canvasController;
         }
     };
 
--- untangly.js
+++ untangly.js
@@ -1,4 +1,7 @@
-/*
+/*jslint maxlen: 80 */
+/**
+ * TODO update documentation
+ *
  * @namespace U
  */
 var U = U || {};
@@ -40,7 +43,7 @@ U.Shape = (function () { // MVC: model
         }
 
         this.preview = previewPath;
-        this.path = shapePath;
+        this.shape = shapePath;
 
     };
 
@@ -89,6 +92,10 @@ U.Node = (function () { // MVC: model
         this.draw = function draw(Shape) {
             this.shape = Shape;
         };
+
+        this.undraw = function undraw() {
+            this.shape = undefined;
+        };
     };
 }());
 
@@ -122,6 +129,8 @@ U.Canvas = (function () {
             if (!grid[x][y]) {
                 grid[x][y] = Node;
             }
+
+            // TODO throw error if program attempts to overwrite existing node
         };
 
         this.remove = function remove(Node) {
@@ -149,358 +158,66 @@ U.CanvasController = (function () {
     "use strict";
 
     return function CanvasController(Canvas, CanvasView) {
-        
+
         if (!(this instanceof U.CanvasController)) {
             return new U.CanvasController(Canvas, CanvasView);
         }
 
+        this.model = Canvas;
+
+        this.draw = function draw(Node, Shape) {
+            Node.draw(Shape);
+
+            // Add new Node objects to empty coordinates surrounding Node object with assigned Shape object
+            this.model.add(new U.Node(Node.x, Node.y + 1)); // north
+            this.model.add(new U.Node(Node.x + 1, Node.y)); // east
+            this.model.add(new U.Node(Node.x, Node.y - 1)); // south
+            this.model.add(new U.Node(Node.x - 1, Node.y)); // west
+        };
+
+        this.undraw = function undraw(Node) {
+            Node.undraw();
+
+            // Remove orphaned Node objects that are left behind
+            this.remove(this.model.grid[Node.x][Node.y + 1]); // north
+            this.remove(this.model.grid[Node.x + 1][Node.y]); // east
+            this.remove(this.model.grid[Node.x][Node.y - 1]); // south
+            this.remove(this.model.grid[Node.x - 1][Node.y]); // west
+        };
+
+        /**
+         * Removes a Node object from the Canvas object after checking that it:
+         * 1. Does not have an attached Shape object
+         * 2. Is not adjacent to a Node object with an attached Shape object
+         *
+         * TODO throw error if program tries to remove a Node object with an
+         *  attached Shape object.
+         *
+         * @method remove
+         *
+         * @param {Node} The Node object to remove from the Canvas object
+         */
+        this.remove = function remove(Node) {
+            var northShape = this.model.grid[Node.x] &&
+                    this.model.grid[Node.x][Node.y + 1] &&
+                    this.model.grid[Node.x][Node.y + 1].shape,
+                eastShape = this.model.grid[Node.x + 1] &&
+                    this.model.grid[Node.x + 1][Node.y] &&
+                    this.model.grid[Node.x + 1][Node.y].shape,
+                southShape = this.model.grid[Node.x] &&
+                    this.model.grid[Node.x][Node.y - 1] &&
+                    this.model.grid[Node.x][Node.y - 1].shape,
+                westShape = this.model.grid[Node.x - 1] &&
+                    this.model.grid[Node.x - 1][Node.y] &&
+                    this.model.grid[Node.x - 1][Node.y].shape;
+
+            if (!Node.shape &&
+                    !northShape &&
+                    !eastShape &&
+                    !southShape &&
+                    !westShape) {
+                this.model.remove(Node);
+            }
+        };
     };
-}());
-
-U.flowchart = (function () { // MVC: model
-    "use strict";
-
-    // Private properties
-        /**
-         * Stores the objects generated for nodes in the "createNode" method in
-         * terms of their grid (x,y) coordinates such that they can be accessed
-         * through "grid[x][y]".
-         *
-         * @property grid
-         * @type array
-         */
-    var grid = [],
-
-        /**
-         * Holds a reference to the objects generated in the "createNode" and
-         * methods for each node.
-         *
-         * @property node
-         * @type array
-         */
-        node = [],
-
-        /**
-         * Holds the values of the bounds of the canvas recorded as units from
-         * the origin, eg. if bound.west is east of the origin it is recorded
-         * as a negative value.
-         *
-         * @property bound
-         * @type object
-         */
-        bound = {
-            north: 0,
-            east: 0,
-            south: 0,
-            west: 0
-        },
-
-
-    // Private methods
-        /**
-         * Adjusts the value of the given bound by the value indicated.
-         *
-         * When both parameters are omitted all four bounds will be determined
-         * by the active nodes; if no active nodes exist the bounds will be set
-         * to Infinity and all operations relying on bounds will break. Calling
-         * adjustBound without parameters is also inefficient. "adjustBound();"
-         * must only be used when bounds cannot be determined in any other way
-         * and only when at least one node exists.
-         *
-         * Event "model:UpdatedBounds" is fired after successful execution.
-         * Event object for "model:UpdatedBounds" implements Bound.
-         *  interface Bound {
-         *      integer north;
-         *      integer east;
-         *      integer south;
-         *      integer west;
-         *  }
-         *
-         * @method adjustBound
-         *
-         * @param {string} direction [Optional] Valid values are "north",
-         *      "east", "south" and "west".
-         * @param {integer} value [Optional] The value by which the bound should
-         *      be adjusted. Valid range is Infinity > value > -Infinity.
-         */
-        adjustBound = function adjustBound(direction, value) {
-
-            var x = [],
-                y = [],
-                i;
-
-            direction = direction.toLowerCase();
-
-            switch (direction) {
-            case "north":
-                bound.north += value;
-                break;
-
-            case "east":
-                bound.east += value;
-                break;
-
-            case "south":
-                bound.south += value;
-                break;
-
-            case "west":
-                bound.west += value;
-                break;
-
-            default:
-                for (i = node.length; i--;) {
-                    x.push(node[i].x);
-                    y.push(node[i].y);
-                }
-                adjustBound("north", Math.max.apply(Math, y) - bound.north);
-                adjustBound("east", Math.max.apply(Math, x) - bound.east);
-                adjustBound("south", -Math.min.apply(Math, y) - bound.south);
-                adjustBound("west", -Math.min.apply(Math, x) - bound.west);
-                break;
-            }
-
-        },
-
-        /**
-         * Creates a new Single node at the given grid (x, y) coordinates.
-         *
-         * The node's object stored in the grid and node arrays implements
-         * SingleNode.
-         *
-         *  interface SingleNode {
-         *      integer x;
-         *      integer y;
-         *      integer i; // index in node array
-         *  }
-         *
-         * @method createNode
-         *
-         * @param {integer} x Grid x coordinate for new Single node.
-         * @param {integer} y Grid y coordinate for new Single node.
-         */
-        createNode = function createNode(x, y) {
-
-            if (!grid[x]) {
-                grid[x] = [];
-            }
-
-            if (!grid[x][y]) {
-                var singleNode = {
-                        x: x,
-                        y: y,
-                        i: node.length
-                    };
-                node.push(singleNode);
-                grid[x][y] = singleNode;
-            }
-
-        },
-
-        /**
-         * Attaches the supplied shape to the specifed node and in so doing
-         * marks the node as Engaged. This process also creates Single nodes
-         * adjacent to the Engaged node, North, East, South and West, to keep
-         * the application functioning.
-         *
-         * The node's object stored in the grid and node arrays is updated and
-         * implements EngagedNode.
-         *
-         * Event "model:EngagedNode" fired after successful execution.
-         * Event object for "model:SingleNode" implements EngagedNode
-         *
-         *  interface EngagedNode {
-         *      integer x;
-         *      integer y;
-         *      integer i; // index in node array
-         *      string pathData;
-         *  }
-         *
-         * @method engageNode
-         *
-         * @param {integer} x Grid x coordinate of node to make Engaged.
-         * @param {integer} y Grid y coordinate of node to make Engaged.
-         * @param {string} pathData SVG path data describing the shape of the
-         *      Engaged node (see documentation for U.Shapes).
-         */
-        engageNode = function engageNode(x, y, pathData) {
-
-            grid[x][y].pathData = pathData;
-
-            if (y + 1 > bound.north) {
-                adjustBound("north", 1);
-            } 
-            createNode(x, y + 1);
-
-            if (x + 1 > bound.east) {
-                adjustBound("east", 1);
-            }
-            createNode(x + 1, y);
-
-            if (y - 1 < -bound.south) {
-                adjustBound("south", 1);
-            }
-            createNode(x, y - 1);
-
-            if (x - 1 < -bound.west) {
-                adjustBound("west", 1);
-            }
-            createNode(x - 1, y);
-        },
-
-        /**
-         * Removes the specified Engaged node and any freestanding Single nodes
-         * left behind as a result.
-         *
-         * Must only be called on Engaged nodes.
-         *
-         * Event "model:RemovedNode" fired before successful execution.
-         * Event object for "model:RemovedNode" implements EngagedNode (see
-         * documentation for engageNode method).
-         *
-         * @method removeNode
-         *
-         * @param {integer} x Grid x coordinate of Engaged node to remove.
-         * @param {integer} y Grid y coordinate of Engaged node to remove.
-         */
-        removeNode = function removeNode(x, y) {
-
-            if (grid[x][y].shapePath) {
-                var dead = [],
-                    adjacentSingleNodes = 0,
-                    NorthWest = !!(grid[x - 1][y + 1] &&
-                            grid[x - 1][y + 1].shapePath),
-                    NorthEast = !!(grid[x + 1][y + 1] &&
-                            grid[x + 1][y + 1].shapePath),
-                    SouthEast = !!(grid[x + 1][y - 1] &&
-                            grid[x + 1][y - 1].shapePath),
-                    SouthWest = !!(grid[x - 1][y - 1] &&
-                            grid[x - 1][y - 1].shapePath),
-                    l = node.length,
-                    i;
-
-                //                      NN              N = North
-
-                //                 NW   N   NE          E = East
-
-                //             WW   W   x   E   EE      S = South
-
-                //                 SW   S   SE          W = West
-
-                //                     SS               x = removed node
-                if (!grid[x][y + 1].shapePath) { // North
-                    adjacentSingleNodes += 1;
-
-                    if (grid[x][y + 2] && grid[x][y + 2].shapePath) { // North North
-                    } else if (NorthWest) {
-                    } else if (NorthEast) {
-                    } else {
-                        dead.push(grid[x][y + 1]);
-                    }
-                }
-                if (!grid[x + 1][y].shapePath) { // East
-                    adjacentSingleNodes += 1;
-
-                    if (grid[x + 2] && grid[x + 2][y] &&
-                            grid[x + 2][y].shapePath) { // East East
-                    } else if (NorthEast) {
-                    } else if (SouthEast) {
-                    } else {
-                        dead.push(grid[x + 1][y]);
-                    }
-                }
-                if (!grid[x][y - 1].shapePath) { // South
-                    adjacentSingleNodes += 1;
-
-                    if (grid[x][y - 2] && grid[x][y - 2].shapePath) { // South South
-                    } else if (SouthEast) {
-                    } else if (SouthWest) {
-                    } else {
-                        dead.push(grid[x][y - 1]);
-                    }
-                }
-                if (!grid[x - 1][y].shapePath) { // West
-                    adjacentSingleNodes += 1;
-
-                    if (grid[x - 2] && grid[x - 2][y] &&
-                            grid[x - 2][y].shapePath) { // West West
-                    } else if (SouthWest) {
-                    } else if (NorthWest) {
-                    } else {
-                        dead.push(grid[x - 1][y]);
-                    }
-                }
-
-                dead.push(grid[x][y]);
-
-                for (i = dead.length; i--;) {
-                    grid[dead[i].x][dead[i].y] = undefined;
-
-                    l -= 1;
-                    node[l].i = dead[i].i;
-                    node[dead[i].i] = node[l];
-                    node.pop();
-                }
-
-                if (adjacentSingleNodes < 4 || // removed node was adjacent to Engaged node
-                        l === 0) { // no nodes remain and program has stopped
-                    createNode(x, y);
-                }
-
-                adjustBound();
-            }
-
-        };
-
-    // End var
-
-
-    // Initialisation proceedures
-
-}());
-
-U.flowchartController = (function () { // MVC: controller
-
-    // Private methods
-    var addSingleBehaviour = function addSingleBehaviour(node) {
-
-            $(node.shape).click(function () {
-
-                /*observer.trigger("controller:SingleNode", {
-                    active: 1,
-                    x: node.x,
-                    y: node.y
-                });*/
-
-            });
-
-        },
-
-        addEngagedBehaviour = function addEngagedBehaviour(node) {
-
-            $(node.shape).click(function () {
-
-                /*observer.trigger("controller:EngagedNode", {
-                    active: 1,
-                    x: node.x,
-                    y: node.y
-                });*/
-
-            });
-
-        };
-
-    // End var
-
-
-    // Initialisation proceedures
-
-    // XXX SAFER IF DONE THROUGH MODEL
-    /*observer.trigger("model:UpdatedBounds", { // initialise canvas start size for MVC: view
-        north: 0,
-        east: 0,
-        south: 0,
-        west: 0
-    });*/
-
 }());
